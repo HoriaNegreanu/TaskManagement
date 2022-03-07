e383486560c429c311d41631003b5fcb66e4b40e
HoriaNegreanu <91976484+HoriaNegreanu@users.noreply.github.com>
2022-03-07 14:03:42 +0200

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TaskManagement.Models;
+
+namespace TaskManagement.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TaskManagement.Models.Movie>? Movie { get; set; }
+    }
+}
---
+++ Models/SeedRoles.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TaskManagement.Data;
+
+namespace TaskManagement.Models
+{
+    public class SeedRoles
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            var context = new ApplicationDbContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>());
+
+            string[] roles = new string[] { "Administrator", "User" };
+
+            foreach (string role in roles)
+            {
+                var roleStore = new RoleStore<IdentityRole>(context);
+
+                if (!context.Roles.Any(r => r.Name == role))
+                {
+                    roleStore.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}

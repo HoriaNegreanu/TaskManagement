bdee1c74c88aa7dd07883ff010d3a854adca161a
HoriaNegreanu <91976484+HoriaNegreanu@users.noreply.github.com>
2022-05-11 15:45:13 +0300

    Modifed Home Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using TaskManagement.Models;
@@ -8,10 +9,12 @@ namespace TaskManagement.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -28,6 +31,21 @@ namespace TaskManagement.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Authorize]
+        public async Task<IActionResult> ViewAllTaskForUser()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            var name = user.FirstName + " " + user.LastName;
+            return RedirectToAction("Index", "TaskItems", new { TaskAssignedTo = name });
+        }
+
+        [Authorize]
+        public async Task<IActionResult> ViewUserActivity()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            return RedirectToAction("Details", "Employees", new { UserId = user.Id });
         }
     }
 }
